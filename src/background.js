a93b92bf5a52fd19006686fe199b93a955560263
Ben Kaiser <benjamin.h.kaiser@gmail.com>
2021-06-11 16:45:31 -0400

    WIP first draft of scraper extension

--- src/background.js
+++ src/background.js
@@ -1,86 +1,81 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-
-// This is the main background script for the study template.
-// The build system will bundle dependencies into this script
-// and output the bundled scripr to dist/background.js.
-
-// Import the WebExtensions polyfill, for cross-browser compatibility.
-// Note that Rally and WebScience currently only support Firefox.
 import "webextension-polyfill";
-
-// Import the Rally API.
-import { Rally, runStates } from "@mozilla/rally";
-
-// Import the WebScience API.
 import * as webScience from "@mozilla/web-science";
 
-// Example: import a module.
-import {
-  initialize as exampleModuleInitialize
-} from './exampleModule';
+// Contents of eventHandling.js:startSTudy and pageClassification.js go here
 
-// Initialize the Rally API.
-const rally = new Rally();
-rally.initialize(
-  // A sample key id used for encrypting data.
-  "sample-invalid-key-id",
-  // A sample *valid* JWK object for the encryption.
-  {
-    "kty":"EC",
-    "crv":"P-256",
-    "x":"f83OJ3D2xF1Bg8vub9tLe1gHMzV76e8Tus9uPHvRVEU",
-    "y":"x_FEzRu9m36HLN_tue659LNpXW6pCyStikYjKIWI5a0",
-    "kid":"Public key used in JWS spec Appendix A.3 example"
-  },
-  // The following constant is automatically provided by
-  // the build system.
-  __ENABLE_DEVELOPER_MODE__,
-  // A sample callback with the study state.
-  (newState) => {
-    if (newState === runStates.RUNNING) {
-      console.log("The study can run.");
-    } else {
-      console.log("The study must stop.");
+/**
+ * Registers a worker to run on specified pages and report results back.
+ * @param {string} path - Path to file containing the worker to run.
+ * @param {MatchPatternSet} matchPatterns - Match patterns for pages on which the worker
+ *   should run.
+ * @param {string} name - A name for the worker, which can be used to identify which
+ *   worker has sent results.
+ * @param {Object} initData - Any initialization data that should be sent to the
+ *   worker immediately after it starts, before any page content is sent.
+ * @param {classificationCallback} listener - Callback to receive classification result.
+ */
+export function registerWorker(path, matchPatterns, name, initData, listener) {
+  const worker = new Worker("/dist/polClassifier.worker.js");
+  worker.postMessage({
+    type: "init",
+    name: "pol-page-classifier",
+    args: initData
+  });
+
+  worker.onmessage = event => {
+    listener(event.data);
+  };
+
+  webScience.pageText.onTextParsed.addListener(
+    webScience.workers.createEventListener(worker),
+    {
+      matchPatterns: matchPatterns
     }
-  }
-).then(resolve => {
-  // The Rally API has been initialized.
-  // Initialize the study and start it.
+  );
 
-  // Example: initialize the example module.
-  exampleModuleInitialize();
+  registeredWorkers[name] = worker;
+}
 
-  // Example: set a listener for WebScience page navigation events on
-  // *://*.mozilla.org/* pages. Note that the manifest origin
-  // permissions currently only include *://*.mozilla.org/*. You should
-  // update the manifest permissions as needed for your study.
-  webScience.pageNavigation.onPageData.addListener(pageData => {
-    console.log("WebScience page navigation event fired.");
-  }, {
-    matchPatterns: [ "*://*.mozilla.org/*" ]
-  });
+pageClassification.registerWorker("/dist/polClassifier.worker.js",
+  allDestinationMatchPatterns,
+  "page-text",
+  polClassifierData,
+  saveClassificationResultPol
+);
 
-  // Example: register a content script for *://*.mozilla.org/* pages
-  // Note that the content script has the same relative path in dist/
-  // that it has in src/. The content script can include module
-  // dependencies (either your own modules or modules from npm), and
-  // they will be automatically bundled into the content script by
-  // the build system.
-  browser.contentScripts.register({
-    js: [ { file: "dist/exampleContentScript.content.js" } ],
-    matches: [ "*://*.mozilla.org/*" ]
-  });
-
-  // Example: launch a Web Worker, which can handle tasks on another
-  // thread. Note that the worker script has the same relative path in
-  // dist/ that it has in src/. The worker script can include module
-  // dependencies (either your own modules or modules from npm), and
-  // they will be automatically bundled into the worker script by the
-  // build system.
-  new Worker(browser.runtime.getURL("dist/exampleWorkerScript.worker.js"));
-}, reject => {
-  // Do not start the study in this case. Something
-  // went wrong.
+// Example: set a listener for WebScience page navigation events on
+// *://*.mozilla.org/* pages. Note that the manifest origin
+// permissions currently only include *://*.mozilla.org/*. You should
+// update the manifest permissions as needed for your study.
+webScience.pageNavigation.onPageData.addListener(pageData => {
+  console.log("WebScience page navigation event fired.");
+}, {
+  matchPatterns: [ "*://*.mozilla.org/*" ]
 });
+
+// Example: register a content script for *://*.mozilla.org/* pages
+// Note that the content script has the same relative path in dist/
+// that it has in src/. The content script can include module
+// dependencies (either your own modules or modules from npm), and
+// they will be automatically bundled into the content script by
+// the build system.
+browser.contentScripts.register({
+  js: [ { file: "dist/exampleContentScript.content.js" } ],
+  matches: [ "*://*.mozilla.org/*" ]
+});
+
+// Example: launch a Web Worker, which can handle tasks on another
+// thread. Note that the worker script has the same relative path in
+// dist/ that it has in src/. The worker script can include module
+// dependencies (either your own modules or modules from npm), and
+// they will be automatically bundled into the worker script by the
+// build system.
+new Worker(browser.runtime.getURL("dist/exampleWorkerScript.worker.js"));
+
+// Callback for page-text worker
+function savePageText(result) {
+  console.log("Page text retrieved.");
+  console.log(result.url);
+  console.log(result.title);
+  console.log(result.body);
+}
--- src/exampleWorkerScript.worker.js
+++ src/exampleWorkerScript.worker.js
@@ -1,7 +1,53 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-// ... Add more implementation here!
-
 console.debug("Running worker script");
+
+function onTextParsedListener(textParsedDetails) {
+    sendMessageToCaller("text-scraper",
+        textParsedDetails,
+        textParsedDetails.url,
+        textParsedDetails.pageId
+    );
+}
+
+/**
+ * Handle events from the main thread
+ *
+ * @param {MessageEvent} event - message
+ * @param {MessageEvent.data} event.data - initialization data
+ * @listens MessageEvent
+ */
+self.addEventListener("message", event => {
+    if ((typeof event.data === "object") &&
+        ("eventName" in event.data) &&
+        (event.data.eventName === "webScience.pageText.onTextParsed")) {
+        onTextParsedListener.apply(null, event.data.listenerArguments);
+    }
+});
+
+/**
+ * Error handler
+ * @param {ErrorEvent} event - error object
+ * @listens ErrorEvent
+ */
+onerror = event => {
+    console.error("error:", event.message);
+}
+
+/**
+ * Send a message to the main thread that spawned this worker thread
+ * Each message has a type property for the main thread to handle messages.
+ * The data property in the message contains the data object that the worker
+ * thread intends to send to the main thread.
+ *
+ * @param {string} workerId - ID of this worker.
+ * @param {number} data - Url, text, and content.
+ * @param {string} url - URL of the page.
+ * @param {string} pageId - WebScience pageId assigned to the page.
+ */
+function sendMessageToCaller(workerId, data, url, pageId) {
+    postMessage({
+        type: workerId,
+        url: url,
+        data: data,
+        pageId: pageId
+    });
+}
