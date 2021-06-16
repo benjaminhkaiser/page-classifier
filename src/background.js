5d11ef4a332678011c42690cfce1336065fb57c6
Ben Kaiser <benjamin.h.kaiser@gmail.com>
2021-06-16 21:39:14 +0000

    getting page contents, just need to save to disk

--- src/background.js
+++ src/background.js
@@ -1,81 +1,36 @@
-import "webextension-polyfill";
-import * as webScience from "@mozilla/web-science";
+import { pageText, workers } from "@mozilla/web-science";
 
-// Contents of eventHandling.js:startSTudy and pageClassification.js go here
+const registeredWorkers = {}
 
-/**
- * Registers a worker to run on specified pages and report results back.
- * @param {string} path - Path to file containing the worker to run.
- * @param {MatchPatternSet} matchPatterns - Match patterns for pages on which the worker
- *   should run.
- * @param {string} name - A name for the worker, which can be used to identify which
- *   worker has sent results.
- * @param {Object} initData - Any initialization data that should be sent to the
- *   worker immediately after it starts, before any page content is sent.
- * @param {classificationCallback} listener - Callback to receive classification result.
- */
-export function registerWorker(path, matchPatterns, name, initData, listener) {
-  const worker = new Worker("/dist/polClassifier.worker.js");
-  worker.postMessage({
-    type: "init",
-    name: "pol-page-classifier",
-    args: initData
-  });
+const worker = new Worker("/dist/pageContentsWorkerScript.worker.js");
+const name = 'text-scraper';
+const listener = savePageText;
+const initData = null;
+const matchPatterns = ["<all_urls>"];
 
-  worker.onmessage = event => {
-    listener(event.data);
-  };
+worker.postMessage({
+  type: "init",
+  name: name,
+  args: initData
+});
 
-  webScience.pageText.onTextParsed.addListener(
-    webScience.workers.createEventListener(worker),
-    {
-      matchPatterns: matchPatterns
-    }
-  );
+worker.onmessage = event => {
+  listener(event.data);
+};
 
-  registeredWorkers[name] = worker;
-}
-
-pageClassification.registerWorker("/dist/polClassifier.worker.js",
-  allDestinationMatchPatterns,
-  "page-text",
-  polClassifierData,
-  saveClassificationResultPol
+pageText.onTextParsed.addListener(
+  workers.createEventListener(worker),
+  {
+    matchPatterns: matchPatterns
+  }
 );
 
-// Example: set a listener for WebScience page navigation events on
-// *://*.mozilla.org/* pages. Note that the manifest origin
-// permissions currently only include *://*.mozilla.org/*. You should
-// update the manifest permissions as needed for your study.
-webScience.pageNavigation.onPageData.addListener(pageData => {
-  console.log("WebScience page navigation event fired.");
-}, {
-  matchPatterns: [ "*://*.mozilla.org/*" ]
-});
-
-// Example: register a content script for *://*.mozilla.org/* pages
-// Note that the content script has the same relative path in dist/
-// that it has in src/. The content script can include module
-// dependencies (either your own modules or modules from npm), and
-// they will be automatically bundled into the content script by
-// the build system.
-browser.contentScripts.register({
-  js: [ { file: "dist/exampleContentScript.content.js" } ],
-  matches: [ "*://*.mozilla.org/*" ]
-});
-
-// Example: launch a Web Worker, which can handle tasks on another
-// thread. Note that the worker script has the same relative path in
-// dist/ that it has in src/. The worker script can include module
-// dependencies (either your own modules or modules from npm), and
-// they will be automatically bundled into the worker script by the
-// build system.
-new Worker(browser.runtime.getURL("dist/exampleWorkerScript.worker.js"));
+registeredWorkers[name] = worker;
 
 // Callback for page-text worker
 function savePageText(result) {
   console.log("Page text retrieved.");
   console.log(result.url);
-  console.log(result.title);
-  console.log(result.body);
+  console.log(result.data.title);
+  console.log(result.data.textContent);
 }
---
+++ src/pageContentsWorkerScript.worker.js
@@ -0,0 +1,65 @@
+console.log("workerscript 1");
+
+function onTextParsedListener(textParsedDetails) {
+    sendMessageToCaller("text-scraper",
+        textParsedDetails,
+        textParsedDetails.url,
+        textParsedDetails.pageId
+    );
+}
+
+/**
+ * Send a message to the main thread that spawned this worker thread
+ * Each message has a type property for the main thread to handle messages.
+ * The data property in the message contains the data object that the worker
+ * thread intends to send to the main thread.
+ *
+ * @param {string} workerId - ID of this worker.
+ * @param {number} data - Url, text, and content.
+ * @param {string} url - URL of the page.
+ * @param {string} pageId - WebScience pageId assigned to the page.
+ */
+function sendMessageToCaller(workerId, data, url, pageId) {
+    postMessage({
+        type: workerId,
+        url: url,
+        data: data,
+        pageId: pageId
+    });
+}
+
+/**
+ * Handle events from the main thread
+ *
+ * @param {MessageEvent} event - message
+ * @param {MessageEvent.data} event.data - initialization data
+ * @listens MessageEvent
+ */
+self.addEventListener("message", event => {
+    console.info("Got event from main thread: ");
+    console.info(event);
+    if ((typeof event.data === "object") &&
+        ("eventName" in event.data) &&
+        (event.data.eventName === "webScience.pageText.onTextParsed")) {
+        onTextParsedListener.apply(null, event.data.listenerArguments);
+    }
+    else if (typeof event.data === "object" &&
+        "type" in event.data &&
+        event.data.type === "init") {
+        initialize(event.data);
+    }
+});
+
+function initialize(initializationData){
+    console.log("Initialized worker.");
+}
+
+/**
+ * Error handler
+ * @param {ErrorEvent} event - error object
+ * @listens ErrorEvent
+ */
+onerror = event => {
+    console.error("error:", event.message);
+}
+
